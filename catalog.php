5e9073fd4a3bc7b6b26586c347b3e4f40ee54b92
Liz-Vazquez <54119354+Liz-Vazquez@users.noreply.github.com>
2020-12-01 23:44:37 -0600

    Cátalogo, ya jala los datos de la bd

--- catalog.php
+++ catalog.php
@@ -2,20 +2,79 @@
 
 require("libs/config.php");
 $pageDetails = getPageDetailsByName($currentPage);
+
 include("header.php");
 ?>
-<div class="row main-row">
-    <div class="8u">
-        <section class="left-content">
-            <h2><?php echo stripslashes($pageDetails["page_title"]); ?></h2>
-            <?php echo stripslashes($pageDetails["page_desc"]); ?>
-        </section>
-    
-    </div>
-     <!--sidebar starts-->
-	<?php include("sidebar.php"); ?>    
-    <!--sidebar ends-->
-</div>
+<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Catálogo</title>
+                    <meta charset="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js?v=<?php echo(rand()); ?>"></script>
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js?v=<?php echo(rand()); ?>"></script>
+                    <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+                    <link rel="stylesheet" href="css/styles.css?v=<?php echo(rand()); ?>">
+                </head>
+                
+                
+                <body class="cuerpo">
+                    <div class="container site">
+                        <h1 class="text-logo"> ¡Adopta una huellita! </h1>
+                        <?php
+                            echo '<nav>
+                                    <ul class="nav nav-pills">';
+
+                            $statement = $DB->query('SELECT * FROM categorias');
+                            $categorias = $statement->fetchAll();
+                            foreach ($categorias as $categoria) 
+                            {
+                                if($categoria['id'] == '1')
+                                    echo '<li role="presentation" class="active"><a href="#'. $categoria['id'] . '" data-toggle="tab">' . $categoria['name'] . '</a></li>';
+                                else
+                                    echo '<li role="presentation"><a href="#'. $categoria['id'] . '" data-toggle="tab">' . $categoria['name'] . '</a></li>';
+                            }
+
+                            echo    '</ul>
+                                </nav>';
+
+                            echo '<div class="tab-content">';
+
+                            foreach ($categorias as $categoria) 
+                            {
+                                if($categoria['id'] == '1')
+                                    echo '<div class="tab-pane active" id="' . $categoria['id'] .'">';
+                                else
+                                    echo '<div class="tab-pane" id="' . $categoria['id'] .'">';
+                                
+                                echo '<div class="row">';
+                                
+                                $statement = $DB->prepare('SELECT * FROM mascota WHERE mascota.categoria = ?');
+                                $statement->execute(array($categoria['id']));
+                                while ($mascota = $statement->fetch()) 
+                                {
+                                    echo '<div class="col-sm-6 col-md-4">
+                                            <div class="thumbnail">
+                                                <img src="ImagenesMascotas/' . $mascota['foto'] . '" alt="...">
+                                                <div class="price">' . $mascota['nombre']. ' </div>
+                                                <div class="caption">
+                                                    <h4>' . $mascota['tipo'] . '</h4>
+                                                    <p>' . $mascota['comentarios'] . '</p>
+                                                    <a href="#" class="btn btn-order" role="button"> Adoptar</a>
+                                                </div>
+                                            </div>
+                                        </div>';
+                                }
+                            
+                            echo    '</div>
+                                    </div>';
+                            }
+                            echo  '</div>';
+                        ?>
+                    </div>
+                </body>
+            </html> 
 <?php
 include("footer.php");
 ?>
